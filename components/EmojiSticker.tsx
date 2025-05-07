90a79ece8b5b930f8c25aee423e8eddfdd33da7d
Meow-Andre <andre.neumann@rocketmail.com>
2025-05-07 13:26:47 +0900

    tutorial finished
    finished the tutorial

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,4 +1,6 @@
 import { View, Image, ImageSourcePropType } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import { Gesture, GestureDetector} from "react-native-gesture-handler";
 
 type EmojiStickerProps = {
   imageSize: number;
@@ -6,13 +8,65 @@ type EmojiStickerProps = {
 };
 
 export default function EmojiSticker({ imageSize, stickerSource }: EmojiStickerProps) {
+  
+const scaleImage = useSharedValue(imageSize);
+const translateX = useSharedValue(0)
+const translateY = useSharedValue(0)
+
+
+const imageStyle = useAnimatedStyle(() => {
+  return {
+    width: withSpring(scaleImage.value),
+    height: withSpring(scaleImage.value),
+  }
+
+});
+
+const doubleTap = Gesture.Tap()
+  .numberOfTaps(2)
+  .onStart(() => {
+    if (scaleImage.value !== imageSize * 2){
+      scaleImage.value = scaleImage.value * 2;
+    } else {
+
+      scaleImage.value = Math.round(scaleImage.value / 2);
+    }
+  }
+)  
+
+const drag = Gesture.Pan().onChange(event => {
+  translateX.value += event.changeX;
+  translateY.value += event.changeY;
+});
+
+const containerStyle = useAnimatedStyle(() => {
+  return {
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        translateY: translateY.value,
+      },
+    ],
+  };
+});
+
+
+
+
   return (
-    <View style={{ top: -350 }}>
-      <Image
+    <GestureDetector gesture={drag}>
+    <Animated.View style={[containerStyle, { top: -350 }]}>
+
+    <GestureDetector gesture={doubleTap}>
+      <Animated.Image
         source={stickerSource}
-        style={{ width: imageSize, height: imageSize }}
+        style={[imageStyle, { width: imageSize, height: imageSize }]}
         resizeMode="contain"
       />
-    </View>
+      </GestureDetector>
+    </Animated.View>
+    </GestureDetector>
   );
 }
